3b99e5938c6f3c442fcff03d6657a69ac37b36ea
cfredberg <53453343+cfredberg@users.noreply.github.com>
2020-09-16 16:56:10 -0700

    guestbook, introtoarraylists IP

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,6 +1,13 @@
 package intro_to_array_lists;
 
-public class GuestBook {
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+
+import javax.swing.*;
+
+public class GuestBook implements ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +16,48 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton addn = new JButton("Add Names");
+	JButton viewn = new JButton("View Names");
+	
+	int counter = 0;
+	
+	ArrayList<String> names = new ArrayList<String>();
+	
+	public static void main(String[] args) {
+		GuestBook g = new GuestBook();
+		g.code();
+	}
+	
+	public void code() {
+		frame.setVisible(true);
+		frame.setPreferredSize(new Dimension(500, 500));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		
+		panel.add(addn);
+		panel.add(viewn);
+		
+		addn.addActionListener(this);
+		viewn.addActionListener(this);
+		
+		frame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == addn) {
+			names.add(JOptionPane.showInputDialog("Name:"));
+		}else if (e.getSource() == viewn) {
+			String answer = "";
+			for (int i = 0; i < names.size(); i++) {
+				counter++;
+				answer += "Guest #" + counter + ": " + names.get(i) + "\n";
+			}
+			JOptionPane.showMessageDialog(null, answer);
+		}
+	}
 }
